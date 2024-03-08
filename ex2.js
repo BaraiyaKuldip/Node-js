782e4feaf547ede8305724a37346f1071b5a066a
BaraiyaKuldip <baraiyakuldip302@gmail.com>
2024-03-08 20:59:32 +0530

    exercise

---
+++ ex2.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var server = http.createServer(function(request,response){
+
+    
+    response.writeHead(200,{'content-type':'text/html'});
+    
+    console.log(request.url);
+    
+    response.write(`<button class='btn' onclick='printTriangle()'>Print Triangle</button><div id='idid'>hii   </div>`);
+    
+    response.end();
+})
+
+server.listen(5000);
+console.log("server is ready ...");
+
+let printTriangle = function (){
+    
+    for (let j = 1; j <= 5; j++)
+    {
+        for (let i = 1; i <= 5; i++)
+        {
+            document.getElementById('idid').innerHTML += "*" ;
+            
+        }
+        document.getElementById('idid').innerHTML += "<br>" ;
+        
+    }
+
+}
+
---
+++ ex3.js
@@ -0,0 +1,14 @@
+
+var i = 0;
+var j = 0;
+var k = 0;
+
+    for (j = 1; j <= 5; j++)
+    {
+        for (i = 1; i <= 5; i++)
+        {
+            console.log("*");
+        }
+        console.log("\n");
+        
+    }
