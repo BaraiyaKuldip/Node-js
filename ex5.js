2b09ce8c19ed76cecb100993e03dfd1ef9ce5cb8
BaraiyaKuldip <baraiyakuldip302@gmail.com>
2024-03-23 10:56:22 +0530

    exercise

--- ex5.js
+++ ex5.js
@@ -4,6 +4,39 @@
 
 var QRCode = require('qrcode')
 
+var express = require('express');
+var app = express();
+
+app.post("/login",function(request,response){
+
+    response.send(" This is Login Page using post method");
+
+});
+
+app.post("/register",function(request,response){
+
+    response.send("This is Register Page using post method ");
+
+});
+
+app.get("/changepassword",function(request,response){
+
+    response.send("This is Change Password Page using get method ");
+});
+
+app.put("/forgotpassword",function(request,response){
+
+    response.send("This is Forgot Password Page using put method");
+
+});
+
+app.delete("/Profile",function(request,response){
+
+    response.send("This is Profile Page using delete method");
+
+});
+
+
 QRCode.toFile('login.png', 'This is Login Page', {
   color: {
     dark: '#008000',  // green dots
@@ -59,3 +92,5 @@ QRCode.toFile('profile.png', 'This is Profile Page', {
   console.log('done')
 });
 
+app.listen(5000);
+console.log("Server is ready..");
